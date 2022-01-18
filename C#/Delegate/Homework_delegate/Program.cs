9532ba29d9a4a6a8c28b6a7ee21d8e373d9a9e05
Julia Ionova <71667162+juliion@users.noreply.github.com>
2022-01-18 14:54:48 +0200

    Add files with delegate task

---
+++ C#/Delegate/Homework_delegate/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Homework_delegate
+{
+    class Program
+    {
+        public static T Find<T>(T[] array, Predicate<T> condition)
+        {
+            foreach (T item in array)
+            {
+                if (condition(item))
+                    return item;
+            }
+            return default(T);
+        }
+        static void Main(string[] args)
+        {
+            int[] arr = new int[10];
+            Random rnd = new Random();
+            for (int i = 0; i < arr.Length; i++)
+                arr[i] = rnd.Next(-20, 20);
+            Console.WriteLine("Array: ");
+            for (int i = 0; i < arr.Length; i++)
+                Console.Write($"{arr[i]} ");
+            Console.WriteLine();
+            Console.Write($"Result of find element < 0: {Find(arr, (elem) => elem < 0)} ");
+            Console.WriteLine();
+        }
+    }
+}
