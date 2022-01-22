7707e79c4ebb6b29fc988897d07559cc1da21902
Julia Ionova <71667162+juliion@users.noreply.github.com>
2022-01-22 15:03:07 +0200

    Add first string task solution files

---
+++ C#/String/Task1/Task1/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter string: ");
+            string str = Console.ReadLine();
+            PrintInfo(str);
+        }
+        static int CountAny(string characters, string str)
+        {
+            int count = 0;
+            foreach (char chStr in str)
+            {
+                foreach (char ch in characters)
+                {
+                    if (chStr == ch)
+                        count++;
+                }
+            }
+            return count;
+        }
+        static int CountLetters(string str)
+        {
+            int count = 0;
+            foreach (char chStr in str)
+            {
+                if (char.IsLetter(chStr))
+                    count++;
+            }
+            return count;
+        }
+
+        static void PrintInfo(string str)
+        {
+            Console.WriteLine("----------------------------");
+            Console.WriteLine("Total сharacters: " + str.Length);
+            Console.WriteLine("Spaces: " + CountAny(" ", str));
+            Console.WriteLine("Digits: " + CountAny("0123456789", str));
+            Console.WriteLine("Punctuation marks: " + CountAny(",.!?-:", str));
+            Console.WriteLine("Letters: " + CountLetters(str));
+            Console.WriteLine("----------------------------");
+        }
+    }
+}
