168c5093ae48db5007dc70a4edd09cde92ff4345
Julia Ionova <71667162+juliion@users.noreply.github.com>
2022-01-22 16:14:46 +0200

    Add fifth string task solution files

---
+++ C#/String/Task5/Task5/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Task5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter string: ");
+            string str = Console.ReadLine();
+            string[] words = str.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+            int ind = 0;
+            string[] newWords = new string[0];
+            foreach (string word in words)
+            {
+                string newWord = word;
+                for (int i = 0; i < newWord.Length; i++)
+                {
+                    char ch = newWord[i];
+                    int lastInd = newWord.LastIndexOf(ch);
+                    if (CountLetter(word, ch) > 1 && i == lastInd)
+                    {
+                        newWord = newWord.Remove(lastInd, 1);
+                        i--;
+                    }
+                }
+                Array.Resize(ref newWords, newWords.Length + 1);
+                newWords[ind] = newWord;
+                ind++;
+            }
+            string newStr = string.Join(" ", newWords);
+            Console.WriteLine("New string: " + newStr);
+        }
+        static int CountLetter(string str, char l)
+        {
+            int count = 0;
+            foreach (char ch in str)
+            {
+                if (ch == l)
+                    count++;
+            }
+            return count;
+        }
+    }
+}
