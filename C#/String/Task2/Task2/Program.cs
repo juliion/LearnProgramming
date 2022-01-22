9845d86b8420f5ad4c95341e9e84fc549667a64f
Julia Ionova <71667162+juliion@users.noreply.github.com>
2022-01-22 16:06:02 +0200

    Add the second string task solution files

---
+++ C#/String/Task2/Task2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter string: ");
+            string str = Console.ReadLine();
+            Console.Write("Enter character: ");
+            char ch = char.Parse(Console.ReadLine());
+            int ind = str.LastIndexOf(ch);
+            string newStr = str.Replace(ch, char.ToUpper(ch)).Substring(0, ind);
+            Console.WriteLine(newStr);
+        }
+    }
+}
